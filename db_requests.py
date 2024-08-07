b0947fc4219081cb39f4d5700a8e956ef4eef3c9
Frozet <32857464+Frozet@users.noreply.github.com>
2024-08-07 18:31:33 +0800

    recolate func for db to separate file

---
+++ db_requests.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db') # Позволяет извлекать данные как словари
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_diagnostics():
+    conn = get_db_connection() 
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM diagnostics")
+    diagnostics = cursor.fetchall()
+    conn.close()
+    return diagnostics
+
+def get_diagnostic_detail(diagnostic_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM diagnostics WHERE id = ?", (diagnostic_id,))
+    diagnostic = cursor.fetchone()
+    conn.close()
+    return diagnostic
+
+def format_diagnostic_data(diagnostic):
+     # Подготовка проблем и расстояний
+    problems = diagnostic['problems'].split(',')
+    distances = diagnostic['problems_distances'].split(',')
+    problem_details = list(zip(problems, distances))  # Создание списка кортежей (проблема, расстояние)
+    # Подготовка даты
+    date_of_diagnostic = diagnostic['date'].split('-')
+    format_date = date_of_diagnostic[2] + '.' + date_of_diagnostic[1] + '.' +  date_of_diagnostic[0] # Форматирование в удобный формат
+    # Подготовка информации о пролетах
+    flows = diagnostic['flow'].split(',')
+    wells = diagnostic['count_of_well'].split(',')
+    wells_firsts = [i for i in wells[::2]]
+    wells_seconds = [i for i in wells[1::2]]
+    distances_between_wells = diagnostic['distance_between_wells'].split(',')
+    slope_between_wells = diagnostic['slope_between_wells'].split(',')
+    wells_details = list(zip(wells_firsts, flows, slope_between_wells, distances_between_wells, wells_seconds)) # Создание списка кортежей
+
+    return problem_details, format_date, wells_details
