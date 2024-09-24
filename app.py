dda64d166ae987acf827148ff733e25b44570ca3
Frozet <32857464+Frozet@users.noreply.github.com>
2024-09-24 14:27:30 +0800

    add about page

--- app.py
+++ app.py
@@ -580,6 +580,11 @@ def annual_report():
     # Рендерим шаблон с данными
     return render_template('annual_report.html', diagnostics=diagnostics, year=year)
 
+# О приложении
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
